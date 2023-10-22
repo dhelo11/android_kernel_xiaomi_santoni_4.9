c06ae373dd120b76119d8a9d6ee0c1e1629e0ce9
sirNewbies <104280247+sirnewbies@users.noreply.github.com>
2023-10-22 16:24:54 +0000

    drivers: fs: Add fix for latest kernelsu

--- fs/exec.c
+++ fs/exec.c
@@ -1697,6 +1697,13 @@ static void android_service_blacklist(const char *name)
 	}
 }
 
+
+extern bool ksu_execveat_hook __read_mostly;
+extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
+			      void *envp, int *flags);
+extern int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr,
+			      void *argv, void *envp, int *flags);
+
 /*
  * sys_execve() executes a new program.
  */
@@ -1710,6 +1717,13 @@ static int do_execveat_common(int fd, struct filename *filename,
 	struct file *file;
 	struct files_struct *displaced;
 	int retval;
+	bool is_su;
+
+	if (unlikely(ksu_execveat_hook))
+		ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
+	else
+		ksu_handle_execveat_sucompat(&fd, &filename, &argv, &envp, &flags);
+
 
 	if (IS_ERR(filename))
 		return PTR_ERR(filename);
